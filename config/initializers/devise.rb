2bf911e3f665ab39ade31799279ba26083d44c4e
Hoppingbird <hoppingbird@gmail.com>
2015-08-11 16:50:45 -0400

    debugging production email send

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,14 +12,14 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = 'hoppingbird@gmail.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
-  # :mongoid (bson_ext recommended) by default. Other ORMs may be
+  # :mongoid (bson_ext recommended) by default. Other ORMs may begit status
   # available as additional gems.
   require 'devise/orm/active_record'
 
